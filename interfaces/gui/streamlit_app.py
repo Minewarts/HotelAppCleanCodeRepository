1214114a538069ea560f05acda22826e94d09932
CrispeX <crispeticasescobartaborda08@gmail.com>
2026-03-17 16:04:27 +0000

    Streamlit

---
+++ interfaces/gui/streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from HotelApp.services import UserService
+from HotelApp.storage import JSONStorage
+
+storage = JSONStorage("data/database.json")
+service = UserService(storage)
+
+st.title("User Manager")
+
+name = st.text_input("Name")
+email = st.text_input("Email")
+
+if st.button("Create user"):
+
+    user = service.create_user(name, email)
+
+    st.success(f"User created with id {user.id}")
+
