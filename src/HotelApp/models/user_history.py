ff9448fd84950a4a98dd56b2a196dfdffa4fd366
CrispeX <crispeticasescobartaborda08@gmail.com>
2026-04-21 16:35:45 +0000

    Correciones

--- src/HotelApp/models/user_history.py
+++ src/HotelApp/models/user_history.py
@@ -9,10 +9,11 @@ class User_History:
     The User_History component is designed to represent and manage the relationship between users and the rooms in which they stay.
     Its primary purpose is to provide a structured and traceable record of room occupancy over time, 
     enabling efficient tracking, querying, and analysis of user-room interactions.
-    
+
     Attributes:
-        _name (str): Name of the hotel.
-        _stars (int | None): Star rating of the hotel.
-        _rooms (List[Room]): List of rooms available in the hotel.
-        _clients (List[User]): List of registered clients.
+        _id (int): Unique identifier of the user.
+        _name (str): Name of the user.
+        check_in: Timestamp indicating when the user entered the room
+        check_out: Timestamp indicating when the user left the room (nullable if ongoing)
     """
+    
